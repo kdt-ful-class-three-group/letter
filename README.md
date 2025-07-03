0452ea1f30a8536bdc56eef9892d1edcf96274f6
김민지 <twoji3019@naver.com>
2025-07-03 12:36:17 +0900

    Feat : README.md에 실행방법 추가

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # 실행 방법
-1. 터미널에 npm run dev 입력
-2. http://localhost:3000 접속
+
+## 1. 의존성 설치
+
+```
+npm install
+```
+## 2. 개발 서버 실행
+
+```
+npm run dev
+```
+
+## 3. 웹사이트 접속
+
+브라우저에서 아래 주소로 접속하세요
+
+[http://localhost:3000](http://localhost:3000)
--- src/script.js
+++ src/script.js
@@ -1,9 +1,42 @@
+let currentIndex = 0
+let messages=[]
+
 async function load(){
   const res = await fetch('/card')
-  const data = await res.json()
+  messages = await res.json()
 
-  const p = document.querySelector('section > p')
-  p.textContent = ''
+  showCard()
 }
 
+function showCard(){
+  const p = document.querySelector('section p')
+  if (currentIndex < messages.length){
+    const msg = messages[currentIndex]
+    p.textContent = msg.content || ''
+
+    // 이미지 위치
+    const imgDiv = document.getElementById('charac')
+    const maxRight = 75; //마지막 위치
+    const step = (maxRight) / (messages.length -1);
+    const newStep = 10 + currentIndex*step
+
+    imgDiv.style.left = `${newStep}%`
+  } else {
+    console.log('카드 완료')
+  }
+  
+}
+
+// 클릭이벤트
+// document.addEventListener('DOMContentLoaded',()=>{
+  const img = document.querySelector('#charac img');
+  img.addEventListener('click',()=>{
+    currentIndex++;
+    if(currentIndex<messages.length){
+      showCard()
+    }
+  })
+// })
+
 load()
+
