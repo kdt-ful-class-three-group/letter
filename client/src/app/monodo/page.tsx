ab37b85112b220a152d2b8beaa8b4cb4bc68031f
최현준 <choijjunii1@gmail.com>
2025-07-07 13:07:22 +0900

    모노두 추가

--- client/src/app/monodo/page.tsx
+++ client/src/app/monodo/page.tsx
@@ -1,8 +1,9 @@
+import { MonodoContainer } from "@/components/monodo/MonodoContainer";
+
 export default function MonodoPage() {
   return (
     <div>
-      <h1>Monodo Page</h1>
-      <p>This is the Monodo page content.</p>
+      <MonodoContainer />
     </div>
   );
 }
